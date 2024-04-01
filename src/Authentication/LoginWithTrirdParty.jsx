385518d961b626dc2d99e7121d63b8f4a97afafe
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-04-01 23:31:49 +0600

    Sign out completed

--- src/Authentication/LoginWithTrirdParty.jsx
+++ src/Authentication/LoginWithTrirdParty.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../../firebase.init";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const LoginWithTrirdParty = () => {
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState({});
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser({});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
@@ -24,8 +40,12 @@ const LoginWithTrirdParty = () => {
         <Helmet>
           <title>Auth | Login</title>
         </Helmet>
-        <button className="btn btn-primary" onClick={handleGoogleSignIn}>
+        <button className="btn btn-primary mx-4" onClick={handleGoogleSignIn}>
           Login
+        </button>
+
+        <button className="btn btn-primary" onClick={handleGoogleSignOut}>
+          Sign Out
         </button>
         <h3>User: {user.displayName}</h3>
         <p>Email: {user.email}</p>
